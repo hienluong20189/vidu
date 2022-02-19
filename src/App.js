44bd86417f3cb7c1bd7af28ae04907d3c77cfeae
Trần Hiền <>
2022-02-20 01:20:41 +0900

    init project

---
+++ src/App.js
@@ -0,0 +1,170 @@
+import "./App.css";
+import React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import {
+  Carousel,
+  CarouselIndicators,
+  CarouselItem,
+  CarouselCaption,
+  CarouselControl,
+} from "reactstrap";
+import Img from "./image/Anh trang chu.png";
+import Img1 from "./image/khach-san-phu-quoc-spa-du-lich-viet.jpeg";
+import Img2 from "./image/Khach-san-vin-osis-du-lich-viet.jpeg";
+
+function App() {
+  return (
+    <div>
+      <div style={{marginBottom: "20px", marginTop: "20px"}} id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img style={{height: "300px"}} src={Img} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img style={{height: "300px"}} src={Img1} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img style={{height: "300px"}} src={Img2} class="d-block w-100" alt="..." />
+          </div>
+        </div>
+        {/* <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button> */}
+      </div>
+
+      <h3>Khách Sạn</h3>
+      <p>
+        <b>Đặt phòng khách sạn :</b>
+        Tổng hợp hơn 200,000 khách sạn trong nước và quốc tế đang có khuyến mãi giá tốt, quý khách
+        có thể dễ dàng tìm kiếm phòng khách sạn cần đặt ngay tại website công ty Du Lịch Việt. Bên
+        cạnh việc có thể đặt mua được phòng khách sạn giá rẻ trực tuyến đơn giản, an toàn, tiết
+        kiệm...quý khách còn có thể tham khảo, so sánh giá phòng khách sạn được cập nhật liên tục
+        tại thời điểm quý khách muốn đặt mua.
+      </p>
+      <h3>Danh Sách Khách Sạn</h3>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <ImageList cols={3}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                title={item.title}
+                subtitle={<span>by: {item.author}</span>}
+                position="below"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </div>
+    </div>
+  );
+}
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "@bkristastucchio",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+    author: "@rollelflex_graphy726",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+    author: "@helloimnik",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+    author: "@nolanissac",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+    author: "@hjrc33",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+    author: "@tjdragotta",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+    author: "@katie_wasserman",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+    author: "@silverdalex",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+    author: "@shelleypauls",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+    author: "@peterlaster",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+    author: "@southside_customs",
+  },
+];
+
+export default App;
